df4be80bfa5af082af5933926e9a4bd059551bb6
Stevy <664553716@qq.com>
2017-12-06 23:42:21 +0800

    change framework

--- linux/storage/__init__.py
+++ linux/storage/__init__.py
@@ -1,27 +1,3 @@
 # coding = utf-8
-import linux
 
 
-def scan_controller():
-	sas2_con = linux.exe_shell("sas2ircu list|grep -P 'SAS[0-9]{4}'")
-	sas3_con = linux.exe_shell("sas3ircu list|grep -P 'SAS[0-9]{4}'")
-	mega_raid_con = linux.exe_shell("storcli show|grep LSI")
-	if "" == sas2_con.strip():
-		indexs = linux.search_regex_strings_column(sas2_con, ".*SAS[0-9]{4}.*", " ", 0)
-		con_str = linux.search_regex_strings(sas2_con, "SAS[0-9]{4}")
-
-
-def scan_from_sas23_disk_name_sn():
-	disks = []
-	tmp = linux.exe_shell("lsblk -o NAME,SERIAL,VENDOR")
-	names = linux.search_regex_strings_column(tmp, "^sd[a-z]+ +([a-z]|[A-Z]|[0-9]| )+", " ", 0)
-	sns = linux.search_regex_strings_column(tmp, "^sd[a-z]+ +([a-z]|[A-Z]|[0-9]| )+", " ", 1)
-	vendors = linux.search_regex_strings_column(tmp, "^sd[a-z]+ +([a-z]|[A-Z]|[0-9]| )+", " ", 2)
-	for i, e in enumerate(names):
-		attr = [
-			names[i],
-			sns[i],
-			vendors[i],
-		]
-		disks.append(attr)
-	return disks
--- linux/storage/phy.py
+++ linux/storage/phy.py
@@ -1,1 +1,68 @@
 # coding = utf-8
+from linux import try_catch
+import linux
+import json
+
+
+class Phy:
+	def __init__(self, t_phy_name):
+		self.phy_name = t_phy_name
+		self.invalid_dword_count = ""
+		self.loss_of_dword_sync_count = ""
+		self.phy_reset_problem_count = ""
+		self.running_disparity_error_count = ""
+		self.sas_address = ""
+	
+	@try_catch
+	def fill_attrs(self):
+		invalid_dword_count_path = "/sys/class/sas_phy/%s/invalid_dword_count" % self.phy_name
+		loss_of_dword_sync_count_path = "/sys/class/sas_phy/%s/loss_of_dword_sync_count" % self.phy_name
+		phy_reset_problem_count_path = "/sys/class/sas_phy/%s/phy_reset_problem_count" % self.phy_name
+		running_disparity_error_count_path = "/sys/class/sas_phy/%s/running_disparity_error_count" % self.phy_name
+		sas_address_path = "/sys/class/sas_phy/%s/sas_address" % self.phy_name
+		self.invalid_dword_count = linux.read_file(invalid_dword_count_path)
+		self.loss_of_dword_sync_count = linux.read_file(loss_of_dword_sync_count_path)
+		self.phy_reset_problem_count = linux.read_file(phy_reset_problem_count_path)
+		self.running_disparity_error_count = linux.read_file(running_disparity_error_count_path)
+		self.sas_address = linux.read_file(sas_address_path)
+	
+	@staticmethod
+	@try_catch
+	def scan_phys():
+		return linux.list_dir_all_files("/sys/class/sas_phy")
+	
+	@staticmethod
+	@try_catch
+	def scan_phys_attr():
+		phy_arr = []
+		for phy in Phy.scan_phys():
+			p = Phy(phy)
+			p.fill_attrs()
+			phy_arr.append(p)
+		return phy_arr
+	
+	@staticmethod
+	@try_catch
+	def scan_err_phys():
+		phy_arr = []
+		for phy in Phy.scan_phys():
+			p = Phy(phy)
+			p.fill_attrs()
+			if int(p.invalid_dword_count) > 0 or int(p.loss_of_dword_sync_count) > 0 or int(
+					p.phy_reset_problem_count) > 0 or int(p.running_disparity_error_count) > 0:
+				phy_arr.append(p)
+		return phy_arr
+	
+	@staticmethod
+	@try_catch
+	def phys_to_json():
+		phy_arr = Phy.scan_phys_attr()
+		phy_json = json.dumps(phy_arr, indent=1)
+		return phy_json
+
+	@staticmethod
+	@try_catch
+	def err_phys_to_json():
+		phy_arr = Phy.scan_err_phys()
+		phy_json = json.dumps(phy_arr, indent=1)
+		return phy_json
