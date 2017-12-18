73c83c5764cdd0704333212b58e4cdb6b4819126
stevy <664553716@qq.com>
2017-12-18 18:31:00 +0800

    修正show_err_disk参数执行过程

--- iads.py
+++ iads.py
@@ -25,18 +25,15 @@ from click_function import *
 	callback=log_all_info,
 	expose_value=False,
 	is_eager=True,
-	help="write all the system info into json file which defind in setting file named log_path."
+	help="Write all the system info into json file which defind in setting file named log_path."
 )
 @click.option(
-	'--write_log',
-	is_flag=True,
-	callback=log_all_info,
-	expose_value=False,
-	is_eager=True,
-	help="write all the system info into json file which defind in setting file named log_path."
+	'--check_env',
+	default='help',
+	help="Check env."
 )
-def main():
-	click.echo('')
+def main(check_env):
+	click.echo('Options:\n  --%s  Show help menu.' % check_env)
 
 if __name__ == '__main__':
 	main()
