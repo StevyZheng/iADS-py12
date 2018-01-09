7b7cee6dda9218f541dca9717cc4f8359cf412a6
stevy <664553716@qq.com>
2018-01-09 18:01:04 +0800

    modify click to fire

--- fire_function.py
+++ fire_function.py
@@ -1,5 +1,5 @@
 # coding = utf-8
-from click_function_tmp import *
+from tmp.click_function_tmp import *
 
 
 class Cli(object):
--- iads.py
+++ iads.py
@@ -1,7 +1,7 @@
 # coding = utf-8
 import fire
 
-from tmp.fire_function import Cli
+from fire_function import Cli
 
 if __name__ == '__main__':
 	fire.Fire(Cli)
